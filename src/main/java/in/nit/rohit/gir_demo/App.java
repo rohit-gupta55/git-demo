07d084368054c45dca9ec26efe18afddedcc0df0
rohit-codeprism <rohit.gupta@codeprism.in>
2020-07-20 12:11:09 +0530

    Project git-demo is added to local repository

---
+++ src/main/java/in/nit/rohit/gir_demo/App.java
@@ -0,0 +1,14 @@
+package in.nit.rohit.gir_demo;
+
+public class App 
+{
+	public void message()
+	{
+		System.out.println("welcome");
+	}
+    public static void main( String[] args )
+    {
+    	new App().message();
+        System.out.println( "ROHIT" );
+    }
+}
